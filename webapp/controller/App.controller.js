5dda659cc04808bb790ab2ac64f93e24619d5e25
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-05-29 17:23:31 -0300

    Step 7: JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-],(Controller,MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+],(Controller,MessageToast,JSONModel) => {
     "use strict";
 
     const AppController = Controller.extend("ui5.walkthrough.controller.App", {
+        
+        onInit(){
+            const data = {
+                recipient : {
+                    name: "World"
+                }
+            }
+
+            this.getView()?.setModel(new JSONModel(data));
+        },
+
         onSayHello(){
             MessageToast.show("Hello World!");
         }
