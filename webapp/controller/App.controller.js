f27d23dab8818746766d56ee3fb9ae5315e21282
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-05-29 17:10:41 -0300

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],(Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],(Controller,MessageToast) => {
     "use strict";
 
     const AppController = Controller.extend("ui5.walkthrough.controller.App", {
         onSayHello(){
-            alert("Hello World Controller");
+            MessageToast.show("Hello World!");
         }
 
     });
