19e7a1924424bbd3a4bbe5b39d2b3cfc7a22380a
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-06-08 11:37:28 -0300

    Step 22: Custom Formatters

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -6,6 +6,20 @@ sap.ui.define([
 
     const Formatter = {
         
+        statusText(status){
+            const resouceBundle = this?.getOwnerComponent()?.getModel("i18n")?.getResourceBundle();
+            
+            switch(status){
+                case "A":
+                    return resouceBundle.getText('invoiceStatusA');
+                case "B":
+                    return resouceBundle.getText('invoiceStatusB');
+                case "C":
+                    return resouceBundle.getText('invoiceStatusC');
+                default:
+                    return status;
+            }
+        }  
     }
 
     return Formatter;
